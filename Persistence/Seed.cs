0135ece9c460a9cbf0f35254666c37d04a27aab3
dk.dwivedi84@gmail.com <dk.dwivedi84@gmail.com>
2021-06-08 22:53:30 -0400

    first commit

---
+++ Persistence/Seed.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if(context.Activities.Any()) return;
+
+            var activities = new List<Activity>{
+                new Activity{
+                    Title="Past Activity 1",
+                    Date=DateTime.Now.AddMonths(-2),
+                    Description="Activity 2 months ago",
+                    Category="drink",
+                    City="London",
+                    Venue="Pub"
+                },
+                new Activity{
+                    Title="Past Activity 2",
+                    Date=DateTime.Now.AddMonths(-1),
+                    Description="Activity 1 month ago",
+                    Category="culture",
+                    City="Peris",
+                    Venue="Louvre"
+                },
+                new Activity{
+                    Title="Futue Activity 1",
+                    Date=DateTime.Now.AddMonths(1),
+                    Description="Activity 1 month ago",
+                    Category="culture",
+                    City="London",
+                    Venue="Natural History Museum"
+                }
+            };
+
+            await context.Activities.AddRangeAsync(activities);
+            await context.SaveChangesAsync();
+        }
+    }
+}
